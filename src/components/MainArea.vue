804ec1e03356f22b1011d7b5611a7512e66f44eb
Nick Gorjestani <nickg2794@hotmail.com>
2021-10-04 17:19:32 -0500

    Added main area component

---
+++ src/components/MainArea.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <title-bar title="Book List"></title-bar>
+    <v-main>
+      <book-form @add-to-list="addBook"></book-form>
+      <book-list :books="books" @remove-book="deleteBook"></book-list>
+      <add-button></add-button>
+    </v-main>
+  </div>
+</template>
+
+<script>
+import TitleBar from "@/components/TitleBar";
+import BookList from "@/components/BookList";
+import AddButton from "@/components/AddButton";
+import BookForm from "@/components/BookForm";
+
+export default {
+  name: "MainArea",
+
+  components: {
+    TitleBar,
+    BookList,
+    AddButton,
+    BookForm,
+  },
+
+  props: {
+    mainComponents: Object,
+  },
+
+  data: () => ({
+    books: [],
+  }),
+
+  computed: {
+
+  },
+
+  methods: {
+    addBook(book) {
+      this.books.push(book);
+    },
+    deleteBook(book) {
+      this.books.splice(this.books.indexOf(book), 1);
+    },
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
